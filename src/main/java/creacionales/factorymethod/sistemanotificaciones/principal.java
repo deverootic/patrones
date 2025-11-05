becadd056c4c84198d6ce134447cb2572aa00088
deverootic <edwin.olaya7@gmail.com>
2025-11-05 17:01:26 -0500

    sendlocal-dev1-cambio3

--- src/main/java/creacionales/factorymethod/sistemanotificaciones/principal.java
+++ src/main/java/creacionales/factorymethod/sistemanotificaciones/principal.java
@@ -28,5 +28,7 @@ public class principal {
         System.out.println("mensaje desde github1");
 
         System.out.println("mensaje desde github2");
+
+        System.out.println("cambio de rama dev1 local");
     }
 }
